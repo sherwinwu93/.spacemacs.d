ddeda90062ba40bc33a79f3d2a33bbb853c0e6e6
root <root@wusd.localdomain>
2022-09-01 13:46:23 +0800

    -

--- wusd/config/init-org.el
+++ wusd/config/init-org.el
@@ -12,19 +12,19 @@
 (setq org-todo-keywords
       '((sequence "TODO(t)"
 		  "DOING(g)"
-		  "WAITING(b)"
-		  "REVIEW(r)"
+		  "BLOCKED(b)"
 		  "|"
 		  "DONE(d)"
+      "CANCELED(c)"
 		  )
 	))
 ;; state的颜色
 (setq org-todo-keyword-faces
-      (quote (("TODO" :foreground "red" :weight bold)
-	      ("DONE" :foreground "forest green" :weight bold)
-	      ("DOING" :foreground "yellow" :weight bold)
-	      ("BLOCKED" :foreground "red" :weight bold)
-	      ("REVIEW" :foreground "orange" :weight bold)
+      (quote (("TODO" :foreground "yellow" :weight bold)
+	            ("DOING" :foreground "spring green" :weight bold)
+	            ("BLOCKED" :foreground "red" :weight bold)
+	            ("DONE" :foreground "forest green" :weight bold)
+	            ("CANCELED" :foreground "yellow" :weight bold)
 	      )))
 
 ;; If task state is turned into done, it will  insert "Closed [timestamp]".
@@ -36,12 +36,18 @@
 (setq org-clock-out-switch-to-state "DONE")
 
 ;; fast tags
-(setq org-tag-alist '(("@work" . ?w)
-		      ("@home" . ?h)
-		      ("@buy" . ?b)
-		      ("@bossurgent" . ?o)
-		      ("@ungroup" . ?u)
-		      ("@study" . ?s)))
+;; (setq org-tag-alist '(("@work" . ?w)
+;; 		      ("@home" . ?h)
+;; 		      ("@buy" . ?b)
+;; 		      ("@bossurgent" . ?o)
+;; 		      ("@ungroup" . ?u)
+;;          ("@study" . ?s)))
+(setq org-tag-alist '(("@task" . ?t)
+		                  ("@work" . ?w)
+		                  ("@idea" . ?i)
+		                  ("@info" . ?f)
+		                  ("@learn" . ?l)
+		                  ))
 
 (setq org-refile-targets '(
 			   ;;修复bug,不可删除
@@ -51,16 +57,44 @@
 			   ))
 (defun agenda-file()
   (interactive)
-  (find-file "~/notes/todos/inbox.org"))
+  (find-file "~/notes/todos/"))
 
 ;; ----------------------------------------capture
-(setq org-capture-templates '(("c" "Todo [Tasks]" entry
-			       (file+headline "~/notes/todos/inbox.org" "Tasks")
-			       "* TODO %i%?\n%T")
-			      ("l" "Todo [NCE2]" entry
-			       (file+headline "~/notes/todos/inbox.org" "NCE2")
-			       "* TODO %i%?\n%T")
-			      ))
+;; (setq org-capture-templates '(
+ ;;("c" "Todo [Tasks]" entry
+;; 			       (file+headline "~/notes/todos/inbox.org" "Tasks")
+;; 			       "* TODO %i%?\n%T")
+;; 			      ("i" "[Ideas]" entry
+;; 			       (file+headline "~/notes/todos/inbox.org" "Ideas")
+;; 			       "* %i%?\n")
+;; 			      ("f" "Todo [Infos]" entry
+;; 			       (file+headline "~/notes/todos/inbox.org" "Infos")
+;; 			       "* %i%?\n")
+			      ;; )
+(setq org-capture-templates
+      '(
+        ("t" "TODO [Task]"
+         entry (file+headline "~/notes/todos/inbox.org" "Task")
+         "* TODO %i%?\n%T"
+         :empty-lines 1)
+        ("w" "TODO [Work]"
+         entry (file+headline "~/notes/todos/inbox.org" "Work")
+         "* TODO %i%?\n%T"
+         :empty-lines 1)
+        ("i" "TODO [Idea]"
+         entry (file+headline "~/notes/todos/inbox.org" "Idea")
+         "* TODO %i%?\n%t"
+         :empty-lines 1)
+        ("f" "[Info]"
+         entry (file+headline "~/notes/todos/inbox.org" "Info")
+         "* %i%?\n"
+         :empty-lines 1)
+        ;; 狭义上的learn
+        ("l" "TODO [learn]"
+         entry (file+headline "~/notes/todos/inbox.org" "Learn")
+         "* TODO %i%?\n%T"
+         :empty-lines 1)
+        ))
 
 (require 'init-org-agenda-custom-commands)
 
