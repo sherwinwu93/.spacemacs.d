cfc6b26261bbd02f82f2c970200e53fa82b6f6d9
root <root@wusd.localdomain>
2023-01-11 15:36:02 +0800

    -

--- lisp/init-translate.el
+++ lisp/init-translate.el
@@ -1,5 +1,7 @@
 (use-package youdao-dictionary
   :load-path "~/.spacemacs.d/lisp/extensions/")
+(setq url-automatic-caching t)
+
 (fset 'wusd/complete-pronounce
       (kmacro-lambda-form [?  ?w ?w ?y ?$ ?  ?w ?w ?p] 0 "%d"))
 (fset 'youdao-quick-translate
